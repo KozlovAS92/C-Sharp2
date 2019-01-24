14fe4cb591c389e43c1d4a2f5fd3cb5311124047
KozlovAS92 <podlisa92@mail.ru>
2019-01-24 12:48:38 +0300

    Задача 1 урока 2 П.С. Закоментил участок кода, не хватило разобраться, почему он не может вернуть переменную.

---
+++ KozlovDZ21.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KozlovDZ21
+{
+    public class Class
+    {
+        public int Metod1(int d, int e, int g)
+        {
+            if ((d < e) && (d < g)) { return d; }
+            else if ((e < d) && (e < g)) { return e; }
+            else { return g; }
+            /*int k;
+            int[] num = { d, e, g };
+            for (int i = 0; i < (num.Length -1); i++) { if (num[i] < num[i + 1]) { k = num[i]; } };
+            return k;*/
+        }
+    }
+    class KozlovDZ21
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выполнение задачи 1:");
+            Console.WriteLine("\nПоследовательно введите три целых числа:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            Class CL = new Class();
+            Console.WriteLine($"Наименьшее число: {CL.Metod1(a, b, c)}");
+            Console.ReadKey();
+        }
+    }
+}
