f5821a8b78d4d677247a0a509f0b798fc387ca33
KozlovAS92 <podlisa92@mail.ru>
2019-01-24 14:26:38 +0300

    Задача 4 Урок 2

---
+++ KozlovDZ24.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KozlovDZ24
+{
+    class Class
+    {
+        string Login = "root";
+        string Password = "GeekBrains";
+        public bool metod1 (string a, string b)
+        {
+            if ((Login == a) && (Password == b)) { return (true); }
+            else { return (false); }
+        }
+    }
+    class KozlovDZ24
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выполнение задачи 4:");
+            string LoginIN = "";
+            string PasswordIN = "";
+            Class CL = new Class();
+            int i = 0;
+            bool Enter = false;
+            do
+            {
+                Console.WriteLine("\nВведите логин: ");
+                LoginIN = Console.ReadLine();
+                Console.WriteLine("\nВведите пароль: ");
+                PasswordIN = Console.ReadLine();
+                if (CL.metod1(LoginIN, PasswordIN)==true) { Enter = true; Console.WriteLine("\nДобро пожаловать!"); break; }
+                else { Console.WriteLine("\nНеверный логин или пароль, попробуйте еще раз."); i++; }
+            }
+            while (i != 3);
+            if (i == 3) { Console.WriteLine("\nКолличество попыток 3. Попробуйте позже"); }
+            Console.ReadKey();
+        }
+    }
+}
